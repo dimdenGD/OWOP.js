41e742428662cae7ca66adb7ea52a5331bfc6b0a
dimdenGD <firefox78888@gmail.com>
2018-12-23 22:09:41 +0200

    fixed bugs

--- examples/gateway.js
+++ examples/gateway.js
@@ -5,10 +5,11 @@ const bot = new Discord.Client();
 
 // Variables
 
-const GuildId = "YOUR_GUILD_ID";
-const ChannelId = "YOUR_GATEWAY_CHANNEL_ID";
-const AdminRole = "ADMIN_ROLE_NAME";
-const BotToken = "YOUR_BOT_TOKEN";
+const GuildId = "YOUR_SERVER_ID";
+const ChannelId = "GATEWAY_CHANNEL_ID";
+const AdminRole = "YOUR_ADMIN_ROLE";
+const BotToken = "BOT_TOKEN";
+const AdminLogin = "ADMIN_LOGIN" // if you have
 
 bot.on("ready", async () => {
     console.log(`\n\x1b[46m`, `${bot.user.username} is online on ${bot.guilds.size} servers!\x1b[0m`);
@@ -23,16 +24,16 @@ bot.on("ready", async () => {
     await OJS.world.move(666666666, 66666666);
     await OJS.interact.controller();
     // < COMMENT THIS CODE IF YOUR OWOP IS NOT ON HEROKU, AND YOU ARE NOT ADMIN
-    OJS.chat.adminlogin('lolno');
-    setInterval(function () {
-       OJS.chat.adminlogin('lolno'); // For not sleepy dinos.
-    },10000);
+    await OJS.chat.adminlogin(AdminLogin);
+    setInterval(async function () {
+       await OJS.chat.adminlogin(AdminLogin); // For not sleepy dinos.
+    },8000);
     // > COMMENT THIS CODE IF YOUR OWOP IS NOT ON HEROKU, AND YOU ARE NOT ADMIN
   });
   OJS.on("message", function (data) {
     OJS.chat.recvModifier(data.data)
     OJS.util.messageHandler(data.data);
-    if(!Buffer.isBuffer(data.data) && !data.data.startsWith('<') && !data.data.startsWith('Connected!') && !data.data.startsWith('[D]') && !data.data.startsWith('Got') && !data.data.startsWith('DEV') && !data.data.startsWith('Nickname') && !data.data.startsWith('Server')) {
+    if(!Buffer.isBuffer(data.data) && !data.data.startsWith('You are') && !data.data.startsWith('FAIL') && !data.data.startsWith('<') && !data.data.startsWith('Connected!') && !data.data.startsWith('[D]') && !data.data.startsWith('Got') && !data.data.startsWith('DEV') && !data.data.startsWith('Nickname') && !data.data.startsWith('Server')) {
   bot.guilds.get(GuildId).channels.get(ChannelId).send(data.data)
   };
   });
@@ -40,22 +41,27 @@ bot.on("ready", async () => {
     console.log('[OWOP.js]: Disconnected.')
   });
 
-bot.on("message", async (message) => {
-  if(message.channel.id == ChannelId) {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+setTimeout(function () {
+  bot.on("message", async (message) => {
+    if(message.channel.id == ChannelId) {
+      message.content = message.content.replace(/<@([0-9]+)>/g, "i'm gay");
+      if (message.author.bot) return;
+      if (message.channel.type === "dm") return;
+      if(message.content.includes('<') && message.content.includes('onerror=') message.content.includes('>')) return;
 
-  OJS.chat.send(`/sayraw [D] ${message.author.username}: ${message.content}`);
-} else if(message.content.startsWith('!OJS')) {
-if(message.member.roles.find("name", AdminRole)) {
-  try {
-  eval(message.content.slice(1));
-  message.reply('Success!');
-} catch(e) {
-  message.reply('Error.');
-}
-} else {message.reply('You are not admin!')};
-}
-});
+      OJS.chat.send(`/sayraw [D] ${message.author.username}: ${message.content}`);
+    } else if(message.content.startsWith('!OJS')) {
+      if(message.member.roles.find("name", AdminRole)) {
+        try {
+          eval(message.content.slice(1));
+          message.reply('Success!');
+        } catch(e) {
+          message.reply('Error.');
+        }
+      } else {message.reply('You are not admin!')};
+    }
+  });
+},3000)
+
 
 bot.login(BotToken);
